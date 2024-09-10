78304c221ddb3c139e969314f101f2875d9f8263
scotymax <smaccord@gmail.com>
2024-09-10 11:25:09 -0400

    Add basic scripts for theme switching

--- static/assets/js/scripts.js
+++ static/assets/js/scripts.js
@@ -12,3 +12,42 @@ function scrollToSection(sectionId) {
 		});
 	}
 }
+function setCookie(name, value, days = 30) {
+	//console.log('setCookie function called');
+	var maxAge = Math.round(days * 86400);
+	var cookieString = 
+		name + '=' + (encodeURIComponent(value) || '') +
+		'; SameSite=Strict' +
+		'; max-age=' + maxAge +
+		'; path=/';
+	document.cookie = cookieString;
+	console.log('Cookie set: ', cookieString);
+}
+function getCookie(name) {
+	//console.log('getCookie function called');
+	var nameEquals = name + '=';
+	var cookieArray = document.cookie.split(';');
+	for (cookie of cookieArray) {
+		while (cookie.charAt(0) == ' ') {
+			cookie = cookie.slice(1, cookie.length);
+		}
+		if (cookie.indexOf(nameEquals) == 0)
+			return decodeURIComponent(
+			cookie.slice(nameEquals.length, cookie.length),
+		);
+	}
+	return null;
+}
+function toggleLightTheme() {
+	// manually add/remove light-theme class from body tag for this view
+	bodyElem = document.getElementsByTagName('body')[0];
+	bodyElem.classList.toggle('light-theme');
+	// if cookie light-theme does not exist, set it
+	if ( lightTheme === null) {
+		setCookie('light-theme', true, 0.125);
+	} else {
+		// if cookie light-theme does exist, set cookie to expire
+		document.cookie = "light-theme= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+	}
+}
+const lightTheme = getCookie('light-theme');
